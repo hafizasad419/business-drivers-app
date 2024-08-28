7d2d1104bdf14a8d5833a84ad7266b1041f4fe11
hafizasad419 <hafizasad419@gmail.com>
2024-08-28 17:40:37 +0500

    update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin:"https://business-drivers-frontend.vercel.app",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+    }
+));
 app.use(bodyParser.json());
 
 // Database connection
