0d2cb0aab8b7e40df090ad5229d7d69b53db615c
hafizasad419 <hafizasad419@gmail.com>
2024-08-28 20:24:59 +0500

    update index

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,21 +10,29 @@ import { configDotenv } from 'dotenv';
 configDotenv();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;  
 
 // Middleware
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+})
+
+
 app.use(cors({
     origin: ['https://business-drivers-frontend.vercel.app', 'http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+
 }));
 
 app.use(bodyParser.json());
 
 // Database connection
 mongoose.connect(process.env.MONGO_DB_URI)
-.then(() => console.log('MongoDB Connected'))
-.catch((err) => console.error('MongoDB connection error:', err));
+    .then(() => console.log('MongoDB Connected'))
+    .catch((err) => console.error('MongoDB connection error:', err));
 
 // Test route
 app.get('/api/test', (req, res) => {
--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 
 function Home() {
   const [apiResponse, setApiResponse] = useState('');
+  const apiUrl = import.meta.env.VITE_API_URL; 
 
   useEffect(() => {
-    axios.get(`${import.meta.env.VITE_API_URL}/test`)
-      .then(response => setApiResponse(response.data.message))
-      .catch(error => console.error('Error fetching the API:', error));
-  }, []);
+    // Call the test API using axios
+    axios.get(`${apiUrl}/api/test`)
+      .then(response => {
+        setApiResponse(response.data.message);
+      })
+      .catch(error => {
+        console.error('Error fetching the API:', error);
+        setApiResponse('Failed to fetch data.');
+      });
+  }, [apiUrl]);
 
   return (
     <div className="App">
@@ -18,7 +25,6 @@ function Home() {
           <li><NavLink to="/">Home</NavLink></li>
           <li><NavLink to="/free-courses">Free Courses</NavLink></li>
           <li><NavLink to="/blogs">Blogs</NavLink></li>
-          <li><NavLink to="/contact-us">Contact Us</NavLink></li>
         </ul>
       </nav>
       <div>
