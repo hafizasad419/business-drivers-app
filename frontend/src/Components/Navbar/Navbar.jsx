23a621173e4d91e720df37348dbea9a247ff086e
hafizasad419 <hafizasad419@gmail.com>
2024-08-28 15:56:06 +0500

    setup navbar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
 import MyButton from '../MyButton/MyButton';
-import mainLogo from "../../assets/Logos/main-logo.webp" 
+import mainLogo from "../../assets/Logos/main-logo.webp"
 function Navbar() {
 
     const navbarItems = [
@@ -31,16 +31,22 @@ function Navbar() {
 
 
     return (
-        <nav className='flex justify-evenly bg-darkBlue text-white
-         py-6 md:min-h-[16vh] items-center'>
+        <nav className='flex justify-around bg-darkBlue text-white
+         py-6 md:py-2 md:min-h-[16vh] items-center'>
 
-
+            <div className="logo w-[18vw] md:w-[8vw] p-0 m-0">
+                <NavLink to="/">
+                    <img className='w-full scale-150' src={mainLogo} alt="" />
+                </NavLink>
+            </div>
 
 
             {/* Middle of Navbar on desktop */}
             <ul className='hidden md:flex justify-evenly'>
                 {navbarItems.map((navItem) => (
-                    <li key={navItem.id}>
+                    <li 
+                    className='mx-8 text-xl'
+                    key={navItem.id}>
                         <NavLink to={navItem.route}>
                             {navItem.title}
                         </NavLink>
