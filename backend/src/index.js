bffbc35e49d204130d95caa906ced01b75c34438
hafizasad419 <hafizasad419@gmail.com>
2024-08-28 17:58:01 +0500

    allow all origins for CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,12 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors(
-    {
-        origin:"*",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-    }
-));
+app.use(cors());
 app.use(bodyParser.json());
 
 // Database connection
